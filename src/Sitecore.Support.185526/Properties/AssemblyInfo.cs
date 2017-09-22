6894f2aaa6895787d90648f0f5c71e3a5e278917
Kateryna Protsenko <kpr@sitecore.net>
2017-09-22 14:24:21 +0300

    #185526: Import source code

---
+++ src/Sitecore.Support.185526/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.185526")]
+[assembly: AssemblyProduct("Sitecore.Support.185526")]
+[assembly: ComVisible(false)]
